bf897be490dc48f6ccd3e2112423ce3c41c20112
DeLuxe <74394136+DeLuxe1337001@users.noreply.github.com>
2022-04-17 00:35:15 -0400

    Update Shutdown.lua

--- load/Shutdown.lua
+++ load/Shutdown.lua
@@ -1,16 +1,18 @@
-while wait() do
-    if _G.HOUR >= 1 or _G.MINUTE >= 1 or _G.SECOND >= 1 then
-        local final_time = (_G.HOUR * 3600) + (_G.MINUTE * 60) + _G.SECOND
-        local current_time = os.time()
+task.spawn(
+    function()
+        while wait() do
+            if _G.HOUR >= 1 or _G.MINUTE >= 1 or _G.SECOND >= 1 then
+                local final_time = (_G.HOUR * 3600) + (_G.MINUTE * 60) + _G.SECOND
+                local current_time = os.time()
 
-        while os.time() - current_time < final_time and wait() do            
-            final_time = (_G.HOUR * 3600) + (_G.MINUTE * 60) + _G.SECOND --Recalculate incase they change.
-            
-            local elapsed = os.time() - current_time
-            _G.ElapsedText.Text = "Elapsed: " .. elapsed .. "/" .. final_time
+                while os.time() - current_time < final_time and wait() do
+                    local elapsed = os.time() - current_time
+                    _G.ElapsedText.Text = "Elapsed: " .. elapsed .. "/" .. final_time
+                end
+
+                print("Finished!")
+                game:Shutdown()
+            end
         end
-
-        print("Finished!")
-        game:Shutdown()
     end
-end
+)
